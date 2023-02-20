1fc77a982497842702bc2d95ba57922761fb928d
elshemmii <43490945+elshemmii@users.noreply.github.com>
2023-02-20 03:05:22 +0200

    users can post and reveal another secrets

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId:String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -61,7 +62,6 @@ passport.use(new GoogleStrategy({
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
-        console.log(profile);
         User.findOrCreate({ googleId: profile.id }, function (err, user) {
             return cb(err, user);
         });
@@ -87,11 +87,38 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 app.get("/secrets", function (req, res) {
+    User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", { usersWithSecrets: foundUsers });
+            }
+        }
+    });
+});
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
-        res.redirect('/login');
+        res.redirect("/login");
     }
+});
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
 app.get("/logout", function (req, res, next) {
     req.logout(function (err) {
@@ -131,6 +158,8 @@ app.post("/login", function (req, res) {
         }
     })
 });
+
+
 
 
 
